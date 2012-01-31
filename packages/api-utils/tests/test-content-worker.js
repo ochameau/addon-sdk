28448b0b4073f6a71eb577790ea45d8c954dd3c7
Alexandre Poirot <poirot.alex@gmail.com>
2012-01-31 16:48:39 +0100

    Ensure that content script API internals doesn't leak to the content script.

--- packages/api-utils/tests/test-content-worker.js
+++ packages/api-utils/tests/test-content-worker.js
@@ -303,6 +303,36 @@ exports['test:chrome is unwrapped'] = function(test) {
 
 }
 
+exports['test:nothing is leaked to content script'] = function(test) {
+  let window = makeWindow();
+  test.waitUntilDone();
+
+  window.addEventListener("load", function onload() {
+    window.removeEventListener("load", onload, true);
+
+    let worker =  Worker({
+      window: window,
+      contentScript: 'new ' + function WorkerScope() {
+        self.postMessage([
+          "ContentWorker" in window,
+          "UNWRAP_ACCESS_KEY" in window,
+          "getProxyForObject" in window
+        ]);
+      },
+      contentScriptWhen: 'ready',
+      onMessage: function(list) {
+        test.assert(!list[0], "worker API contrustor isn't leaked");
+        test.assert(!list[1], "Proxy API stuff isn't leaked 1/2");
+        test.assert(!list[2], "Proxy API stuff isn't leaked 2/2");
+        test.done();
+      }
+    });
+
+  }, true);
+
+}
+
+
 exports['test:setTimeout can\'t be cancelled by content'] = function(test) {
   let contentURL = 'data:text/html,<script>var documentValue=true;</script>';
   let window = makeWindow(contentURL);
